a132180c6deaacf3bf5a29f50b12d390318e8506
Bradley <bradlabrador@hotmail.com>
2018-09-19 22:26:00 -0400

    3 servos working at the same time

--- servoGPIO.py
+++ servoGPIO.py
@@ -1,33 +1,46 @@
 import RPi.GPIO as GPIO
+import time
 
-bottomSignal = 17
-middleSignal = 27
-topSignal = 22
+class ServoGPIO():
+    # GPIO pins for the different servos
+    signalBottom = 17
+    signalMiddle = 27
+    signalTop = 22
+    # Frequency of 50Hz gives period of 20 ms
+    frequency = 50
 
-def servoGPIOSetup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(bottomSignal, GPIO.OUT)
+    def __init__(self):
+        # Use Broadcom SOC channel numbers for pins
+        GPIO.setmode(GPIO.BCM)
+        # Set GPIO pins to optput
+        GPIO.setup(self.signalBottom, GPIO.OUT)
+        GPIO.setup(self.signalMiddle, GPIO.OUT)
+        GPIO.setup(self.signalTop, GPIO.OUT)
+        # Initialize all the PWMs with the frequency
+        self.pwmBottom = GPIO.PWM(self.signalBottom, self.frequency)
+        self.pwmMiddle = GPIO.PWM(self.signalMiddle, self.frequency)
+        self.pwmTop = GPIO.PWM(self.signalTop, self.frequency)
+        # Neutral for HS-322HD is 1.5ms
+        # 2.5% (0 degrees) 12.5% (180 degrees)
+        # Set all servos to starting position
+        self.pwmBottom.start(2.5)
+        self.pwmMiddle.start(2.5)
+        self.pwmTop.start(2.5)
+        time.sleep(2)
 
-def servoTest():
-    # GPIO 17 for PWM with 50Hz
-    pwm = GPIO.PWM(bottomSignal, 50)
-    # Initialization
-    pwm.start(2.5)
-    try:
-        while True:
-            pwm.ChangeDutyCycle(5)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(7.5)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(10)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(12.5)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(10)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(7.5)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(5)
-            time.sleep(0.5)
-            pwm.ChangeDutyCycle(2.5)
-            time.sleep(0.5)
+    def servoGPIOCleanup(self):
+        GPIO.cleanup()
+
+    def servosSet(self, pwm1, pwm2, pwm3, dutyCycle):
+        pwm1.ChangeDutyCycle(dutyCycle)
+        pwm2.ChangeDutyCycle(dutyCycle)
+        pwm3.ChangeDutyCycle(dutyCycle)
+        time.sleep(1)
+
+def main():
+    pablo = ServoGPIO()
+    pablo.servosSet(pablo.pwmBottom, pablo.pwmMiddle, pablo.pwmTop, 12.5)
+    pablo.servoGPIOCleanup()
+
+if __name__ == "__main__":
+    main()
